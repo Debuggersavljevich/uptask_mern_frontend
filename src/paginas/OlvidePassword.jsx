673342495ea00a62d24b0629d28998011dd3fd69
Santiago Bogosavljevich <sbogosavljevich@gmail.com>
2022-10-22 20:06:52 -0300

    reset password function

--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,30 +1,80 @@
+import axios from 'axios'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Alerta from '../components/Alerta'
 
 const OlvidePassword = () => {
+  
+  const [email, setEmail] = useState('')
+  const [alerta, setAlerta] = useState({})
+
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    if(email === '' || email.length < 6){
+      setAlerta({
+        msg: "El email es obligatorio",
+        error: true,
+      })
+      return
+    }
+
+    try {
+      const {data} = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/usuarios/olvide-password`, {email})
+
+      // console.log(data);
+      setAlerta({
+        msg: data.msg,
+        error: false
+      })
+    } catch (error) {
+     setAlerta({
+       msg: error.response.data.msg,
+       error: true
+     }) 
+    }
+      
+    
+  }
+
+  const {msg} = alerta
+
   return (
     <>
-      <h1 className='text-sky-600 font-black text-6xl capitalize'>Recupera acceso a tus {''}
+      <h1 className='text-sky-600 font-black text-6xl capitalize mb-10'>Recupera acceso a tus {''}
         <span className='text-slate-700'>proyectos</span>
       </h1>
-    
-      <form className='my-10 bg-white shadow-2xl rounded-lg p-10'>
-       <div className='my-5'>
-          <label htmlFor='email' className='uppercase text-gray-600 block text-xl font-bold'>Email</label>
-          <input id='email' type="email" placeholder='email de registro' className='w-full mt-3 p-3 border rounded-xl bg-gray-50' />
-        </div>
+
+            {msg && <Alerta alerta={alerta}/>}
+
+          <form className='my-10 bg-white shadow-2xl rounded-lg p-10' onSubmit={handleSubmit}>
+          <div className='my-5'>
+              <label htmlFor='email' className='uppercase text-gray-600 block text-xl font-bold'>Email</label>
+              <input 
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              id='email' 
+              type="email" 
+              placeholder='email de registro' 
+              className='w-full mt-3 p-3 border rounded-xl bg-gray-50' />
+            </div>
 
 
-       <input type="submit" value="Enviar instrucciones" className='bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded-xl hover:cursor-pointer hover:bg-sky-800 transition-all' />
+          <input 
+          type="submit" 
+          value="Enviar instrucciones" 
+          className='bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded-xl hover:cursor-pointer hover:bg-sky-800 transition-all' />
 
-      </form>
+          </form>
 
-      <nav className='lg:flex lg:justify-between'>
-        <Link to="/" className='block text-center my-5 text-slate-500 uppercase text-sm'>
-          ¿Ya tienes una cuenta? Iniciá sesión
-        </Link>
-        
-        <Link to="/" className='block text-center my-5 text-slate-500 uppercase text-sm'>
-          ¿No tienes una cuenta? Registrate
+          <nav className='lg:flex lg:justify-between'>
+            <Link to="/" className='block text-center my-5 text-slate-500 uppercase text-sm'>
+              ¿Ya tienes una cuenta? Iniciá sesión
+            </Link>
+            
+            <Link to="/" className='block text-center my-5 text-slate-500 uppercase text-sm'>
+              ¿No tienes una cuenta? Registrate
         </Link>
       </nav>
     </>
